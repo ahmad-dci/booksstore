07465877a605a9508574322f637d76c050b31994
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-05-15 10:57:30 +0200

    booksstor mongo connect

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,6 +2,62 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const adminRoutes = express.Router();
 
+const books = [
+    {
+      title: 'War and Peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
+    },
+    {
+      title: 'Les Misérables',
+      genre: 'Historical Fiction',
+      author: 'Victor Hugo',
+      read: false
+    },
+    {
+      title: 'The Time Machine',
+      genre: 'Science Fiction',
+      author: 'H. G. Wells',
+      read: false
+    },
+    {
+      title: 'A Journey into the Center of the Earth',
+      genre: 'Science Fiction',
+      author: 'Jules Verne',
+      read: false
+    },
+    {
+      title: 'The Dark World',
+      genre: 'Fantasy',
+      author: 'Henry Kuttner',
+      read: false
+    },
+    {
+      title: 'The Wind in the Willows',
+      genre: 'Fantasy',
+      author: 'Kenneth Grahame',
+      read: false
+    },
+    {
+      title: 'Life On The Mississippi',
+      genre: 'History',
+      author: 'Mark Twain',
+      read: false
+    },
+    {
+      title: 'Childhood',
+      genre: 'Biography',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
+    }];
+    
+const book ={
+    title: 'FBWIIHamburg',
+    genre: 'web development',
+    author: 'ahmad',
+    read: false
+  }
 function route(nav){
     adminRoutes.route('/').get((req, res)=>{
         const url = 'mongodb://localhost:27017';
@@ -11,7 +67,9 @@ function route(nav){
            
            try {
                client = await MongoClient.connect(url);
-               res.send("connection done");
+               const db = client.db(dbName);
+               const response = await db.collection('books').insert(book);
+               res.send(response);
                } catch (error) {
                 res.send(error.message);
                }
