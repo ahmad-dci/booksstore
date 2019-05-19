def1c3e01e12c708008ca582ae60cbfdc39296c1
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-05-19 11:10:17 +0200

    last update

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -76,9 +76,50 @@ function route(nav){
           }());
 
     });
+   //show page
     adminRoutes.route('/addBook').get((req, res)=>{
        res.render('addBook', { nav, title:'Add Book' });
     });
+    //
+    adminRoutes.route('/addBook').post((req, res)=>{
+      //res.render('addBook', { nav, title:'Add Book' });
+      //req.body not gonna work without body parser package
+      let errorMessage="";
+      if(!req.body.bookTitle){
+        errorMessage ="Please enter the Book title <br>";
+      }
+       if(!req.body.bookAuthor){
+        errorMessage +="Please enter the Book Author <br>";
+      } 
+      if(!req.body.bookGenre){
+        errorMessage +="Please enter the Book Genre <br>";
+      }
+      if(errorMessage!=""){
+      res.send(errorMessage);
+      }else{
+        const url = 'mongodb://localhost:27017';
+        const dbName = 'libraryApp';
+        (async function mongo(){
+          let client;
+          try{
+            client = await MongoClient.connect(url, {useNewUrlParser: true});
+            const db = client.db(dbName);
+            const response = await db.collection('books').insert({
+              title: req.body.bookTitle,
+              genre: req.body.bookGenre,
+              author: req.body.bookAuthor,
+              read: false
+            });
+            res.send(response);
+          }  
+          catch(error){
+            res.send(error.message);
+          }
+          client.close();
+        }());
+      }
+      //res.json(req.body.bookTitle);
+   });
     return adminRoutes
 }
 
